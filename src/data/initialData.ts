b5760001f1f96d4ec9258072303846d4e26ddbc1
Pete Dragun <pdragun@phdsolutions.ca>
2024-07-05 14:01:43 -0400

    cleaned up

--- src/data/initialData.ts
+++ src/data/initialData.ts
@@ -1,6 +1,14 @@
 import { Student } from './types';
 
 export const initialStudents: Student[] = [
-  { id: 1, name: 'John', age: 20, sex: 'M', active: true },
+  { id: 1, name: 'Jon', age: 20, sex: 'M', active: true },
   { id: 2, name: 'Jane', age: 22, sex: 'F', active: false },  
+  { id: 3, name: 'Luis', age: 55, sex: 'M', active: true },  
+  { id: 4, name: 'Jesse', age: 30, sex: 'M', active: true },  
+  { id: 5, name: 'Laura', age: 30, sex: 'F', active: false },  
+  { id: 6, name: 'Cesar', age: 30, sex: 'M', active: false },  
+  { id: 7, name: 'Katrina', age: 40, sex: 'F', active: false },  
+  { id: 8, name: 'Paul', age: 40, sex: 'M', active: false },  
+  { id: 9, name: 'Jacob', age: 22, sex: 'M', active: true },  
+  { id: 10, name: 'Chris', age: 44, sex: 'M', active: true }  
 ];
